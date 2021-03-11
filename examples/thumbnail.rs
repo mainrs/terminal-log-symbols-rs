0c58d22961e413908beb63a84e98accfdf042d69
SirWindfield <SirWindfield@users.noreply.github.com>
2021-03-11 04:22:15 +0100

    feat: add colored variants
    The variants are accessible under the `colored` module. They are enabled
    by default and can be disabled through the `colors` feature.

--- examples/thumbnail.rs
+++ examples/thumbnail.rs
@@ -1,8 +1,16 @@
-use terminal_log_symbols::{ERROR_SYMBOL, INFO_SYMBOL, SUCCESS_SYMBOL, WARNING_SYMBOL};
+use terminal_log_symbols::{colored, ERROR_SYMBOL, INFO_SYMBOL, SUCCESS_SYMBOL, WARNING_SYMBOL};
 
 fn main() {
     println!("{} Failed to download file", ERROR_SYMBOL);
     println!("{} Finished downloading file", SUCCESS_SYMBOL);
     println!("{} HDD storage space reaching limit", WARNING_SYMBOL);
     println!("{} Created new repository", INFO_SYMBOL);
+    println!();
+    println!("{} Failed to download file", colored::ERROR_SYMBOL);
+    println!("{} Finished downloading file", colored::SUCCESS_SYMBOL);
+    println!(
+        "{} HDD storage space reaching limit",
+        colored::WARNING_SYMBOL
+    );
+    println!("{} Created new repository", colored::INFO_SYMBOL);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,20 @@ pub const INFO_SYMBOL: Emoji = Emoji::new("ℹ", "i");
 pub const SUCCESS_SYMBOL: Emoji = Emoji::new("✔", "√");
 /// A symbol used to indicate a recoverable error.
 pub const WARNING_SYMBOL: Emoji = Emoji::new("⚠", "‼");
+
+#[cfg(feature = "colors")]
+pub mod colored {
+    use ansi_colors_macro::ansi_string;
+    use terminal_emoji::Emoji;
+
+    /// A symbol used for indicating error messages.
+    pub const ERROR_SYMBOL: Emoji = Emoji::new(ansi_string!("{red ✖}"), ansi_string!("{red ×}"));
+    /// A symbol used for indicating additional information to the user.
+    pub const INFO_SYMBOL: Emoji = Emoji::new(ansi_string!("{blue ℹ}"), ansi_string!("{blue i}"));
+    /// A symbol used for indicating a successful operation.
+    pub const SUCCESS_SYMBOL: Emoji =
+        Emoji::new(ansi_string!("{green ✔}"), ansi_string!("{green √}"));
+    /// A symbol used to indicate a recoverable error.
+    pub const WARNING_SYMBOL: Emoji =
+        Emoji::new(ansi_string!("{yellow ⚠}"), ansi_string!("{yellow ‼}"));
+}
